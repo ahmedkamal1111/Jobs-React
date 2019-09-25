af823128851fd43aa3c87282fda61b4af09d55e9
Beatheat <abdallah.mohamedfcih@gmail.com>
2019-09-25 14:48:25 +0200

    fix conflicts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Dashboard from './components/Dashboard/dashboard'
 
 
 function App() {
   
   return (
     <div >
-      <Dashboard />
+     
     </div>
   );
 
