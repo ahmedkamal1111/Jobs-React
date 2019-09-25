daf1c10d59ae1ac6bdd10c08cd493f5a83a0ac9c
Beatheat <abdallah.mohamedfcih@gmail.com>
2019-09-25 15:13:57 +0200

    fixing conflicts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,180 @@
 
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import { Switch, Route } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
+import LoginPage from './containers/Auth/Login';
+
+class App extends Component {
   
-  return (
-    <div >
-     
-    </div>
-  );
+  constructor(props) {
+    
+    super(props)
+    
+    this.state = {
+      isAuth: false,
+      token: null,
+      adminId: null,
+      authLoading: false,
+      error: null
+    };
+
+  }
+
+  componentDidMount() {
+
+    //Get data authentication from localStorage
+    const token = localStorage.getItem('token');
+    const expiryDate = localStorage.getItem('expiryDate');
+    const adminId = localStorage.getItem('adminId');
+
+    //Check in data existing
+    if ( !token && !expiryDate ) {
+      return;
+    }
+
+    //Check in expiry Date if expiry data is 0 or less than 0 
+    //It return true and logout else skip
+    if ( new Date(expiryDate) <= new Date() ) {
+      this.logout(); //logout
+      return;
+    }
+    
+    //Get the time remaining in seconds
+    const remainingMilliseconds = new Date(expiryDate).getTime() - new Date().getTime();
+
+    //update state
+    this.setState({ setAuth: true, token: token, adminId: adminId});
+
+    //set auto Logout depend on time remaining
+    this.autoLogout(remainingMilliseconds);
+
+  }
+
+  //Logout Func
+  logout = () => {
+    
+    //update state
+    this.setState({isAuth: false ,token: null, adminId: null});
+
+    //Remove date authentication from localstorage
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiryDate');
+    localStorage.removeItem('adminId');
+  
+  }
+  
+  //autoLogout Func
+  autoLogout = remainingMilliseconds => {
+    setTimeout( this.logout() , remainingMilliseconds );
+  }
+
+  //Login Func
+  login = (event, authData) => {
+
+    event.preventDefault();
+
+    //Update state to fire spinner
+    this.setState({ authLoading: true });
+
+    //Post data authentication to login onto the system
+    axios.post("URL Endpoint", { 
+        email: authData.email,
+        password: authData.password    
+    })
+      .then(res => {
+        
+        //Handle Response Status
+        if( res.status === 422 ) {
+          
+          throw new Error("Validation Failed.");
+        
+        } 
+        
+        if ( res.status !== 200 || res.status !== 201 ) {
+
+          throw new Error("Could not authenticate yet, you should input the right password or email");
+        
+        }
+        
+        //udpate state with initial data
+        this.setState({
+          isAuth: true,
+          authLoading: false,
+          token: res.data.token,
+          adminId: res.data.adminId,
+        });
+
+        //Set data authentication onto localStorage
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('adminId', res.data.adminId);
+
+        //set remaining time
+        const remainingMilliseconds = 60 * 60 * 1000; //Minutes * seconds * Milliseconds
+        
+        //Set expiryDate in localStorage
+        const expiryDate = new Date( new Date().getTime() + remainingMilliseconds );
+        localStorage.setItem('expiryDate', expiryDate.toISOString());
+
+        //auto logout depend on time remaining
+        this.autoLogout(remainingMilliseconds);
+
+      })
+      .catch(err => {
+        
+        //update state with initial errors
+        this.setState({
+          isAuth: false,
+          authLoading: false,
+          error: err
+        });
+
+      })
+
+  }
+
+  render () {
+
+    let routes = (
+      
+      <Switch>
+
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <LoginPage
+              {...props}
+              onLogin={this.login}
+              loading={this.state.authLoading}
+            />
+          )}
+        />
+      
+      </Switch>
+    );
+
+    if (this.state.isAuth) {
+      routes = (
+       
+        <Switch>
+          <Route>
+            { /* Dashboard */ }
+          </Route>
+        </Switch>
+
+      );
+    }
+    return (
+      
+      <Fragment>
+      
+        {routes}
+      
+      </Fragment>
+
+    );
+  }
 
 }
 
