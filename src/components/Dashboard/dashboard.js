7bcc412dbfb81a856682907e355e0e8537d78433
Beatheat <abdallah.mohamedfcih@gmail.com>
2019-11-06 16:07:21 +0200

    fix drawer width issue

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -94,9 +94,9 @@ const useStyles = makeStyles(theme => ({
       duration: theme.transitions.duration.leavingScreen,
     }),
     overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
+    // width: theme.spacing(7) + 1,
     [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
+      // width: theme.spacing(9) + 1,
     },
   },
   content: {
