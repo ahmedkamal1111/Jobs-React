bfe656c1e21ac73379babbdeeaab06b947d018aa
Beatheat <abdallah.mohamedfcih@gmail.com>
2019-11-06 11:10:21 +0200

    createjob/intern fixes

--- src/components/Form/Internship/addIntern.js
+++ src/components/Form/Internship/addIntern.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import '../form.css';
+import {
+    Heading,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button
+} from "@chakra-ui/core";
+import Select from "react-select";
 
 export default class CreateInternship extends Component {
-    componentDidMount(){
-        Axios.get("https://joblaravel.tbv.cloud/jobtypes")
-        .then(response => {
-            console.log(response.data.filter(i => i.id === 5))
-        })
+    constructor() {
+        super()
+        this.state = {
+            internship: [],
+        }
     }
-    render(){
-        return(<h1>tmam</h1>)
+    componentDidMount() {
+        Axios.get("https://joblaravel.tbv.cloud/jobtypes")
+            .then(response => {
+                console.log(response.data.filter(i => i.id === 5))
+                const internship = response.data.filter(i => i.id === 5)
+                this.setState(prevState => ({
+                    ...prevState,
+                    internship
+                }))
+
+            })
+    }
+
+    internData = () => {
+        return (
+            this.state.internship.map((option, k) => {
+                return <option value={option.id}></option>
+            }))
+    }
+    render() {
+
+        return (
+            <div className="content-form">
+                <div className="container">
+                    <Heading className="jobsHead">Add an internship</Heading>
+                    <select className="form-control" disabled>
+                        {this.state.internship.map((item, k) => {
+                            return <option selected value={item.id}>{item.name}</option>
+                        })}
+                    </select>
+                    <input type="text" placeholder="Speciality" className="form-control mt-3"></input>
+                  
+
+
+                </div>
+            </div>
+
+        );
     }
 }
